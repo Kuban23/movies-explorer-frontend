3c53641adfee93f996b28766071b05def18c4e41
Esin Andrew <kubanesin@gmail.com>
2022-06-01 00:14:51 +0300

    feat: Сверстал Header 1280px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,22 @@
 
 import React from "react";
 import './Header.css'
+import logo from '../../images/logo.svg';
 
-function Header(){
-	return ( 
-<div></div>
-);
+function Header() {
+   return (
+      <header className="header">
+         <img className="header__logo" src={logo} />
+
+         <nav className="header__link-container">
+            <div className="header__link-auth header__link-auth_type_up">Регистрация</div>
+            <button className="header__link-auth header__link-auth_type_in">Войти</button>
+         </nav>
+
+
+
+      </header>
+   );
 }
 
 export default Header;
