50e5adb0a8e5e3d99ea119d93dbfe51d991c52d3
Esin Andrew <kubanesin@gmail.com>
2022-05-31 21:51:23 +0300

    feat: Скачал image

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
-// import reportWebVitals from './reportWebVitals';
+// react 17.0.2
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-//   <React.StrictMode>
-    <App />
-//   </React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './components/App';
+// // import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+// //   <React.StrictMode>
+//     <App />
+// //   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
