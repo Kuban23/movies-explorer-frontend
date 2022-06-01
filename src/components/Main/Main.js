3731cb47bf8c90aa9952c9a671630879246e1529
Esin Andrew <kubanesin@gmail.com>
2022-06-01 21:22:53 +0300

    feat: Сверстал AboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './Main.css'
 import Promo from "../Promo/Promo";
-
+import AboutProject from "../AboutProject/AboutProject";
 
 function Main(){
 	return ( 
   <div>
   <Promo />
-
+  <AboutProject/>
   </div>
 
 );
