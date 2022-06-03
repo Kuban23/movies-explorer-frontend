cce9ea22877a9779896ea2e6a5705e4c238001c8
Esin <kubanesin@gmail.com>
2022-06-03 10:38:58 +0300

    feat:Сверстал Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,10 +1,37 @@
 
 import React from "react";
 import './Portfolio.css'
+import ArrowImage from '../../images/arrow.svg';
 
 function Portfolio(){
 	return ( 
-<div></div>
+<section className="portfolio">
+<h2 className="portfolio__title">Портфолио</h2>
+<ul className="portfolio__links">
+	<li className="portfolio__item">
+	<a className="portfolio__link" href="#">
+	<h3 className="portfolio__link-title">Статичный сайт</h3>
+	<img className="portfolio__link-picture" src={ArrowImage} alt="Изображение стрелки" />
+	</a>
+	</li>
+
+	<li className="portfolio__item">
+	<a className="portfolio__link" href="#">
+	<h3 className="portfolio__link-title">Адаптивный сайт</h3>
+	<img className="portfolio__link-picture" src={ArrowImage} alt="Изображение стрелки" />
+	</a>
+	</li>
+
+	<li className="portfolio__item">
+	<a className="portfolio__link" href="#">
+	<h3 className="portfolio__link-title">Одностраничное приложение</h3>
+	<img className="portfolio__link-picture" src={ArrowImage} alt="Изображение стрелки" />
+	</a>
+	</li>
+
+</ul>
+
+</section>
 );
 }
 
