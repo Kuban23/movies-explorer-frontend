d257684504ff796f31de560a5a86f95a45e605bf
Esin <kubanesin@gmail.com>
2022-06-08 10:50:40 +0300

    feat:Реализовал роуты по клику logo и /movies

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,22 @@
 
 import React from "react";
 import './Header.css'
-import logo from '../../images/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import icon from '../../images/profile_icon.svg';
 
 function Header() {
    return (
       <header className="header">
          
-         <img className="header__logo" src={logo} alt="Логотип проекта" />
+         <Link className="header__logo" alt="Логотип проекта" to="/"/>
 
          <nav className="header__links">
-         <Link className="header__link-films">Фильмы</Link>
-         <Link className="header__link-films">Сохранённые фильмы</Link>
+         <Link className="header__link-films" to="/movies">Фильмы</Link>
+         <Link className="header__link-films" to="/saved-movies">Сохранённые фильмы</Link>
          </nav>
       <>
       
-         <Link className="header__profile-link">
+         <Link className="header__profile-link" to="/profile">
          <img className='header__profile-link-icon'src={icon} alt='Иконка профиля'/>
          </Link>
          <button className="header__menu-button" type="button"></button>
@@ -26,13 +25,10 @@ function Header() {
          {/* <nav className="header__link-container">
             <Link className="header__link-auth header__link-auth_type_up">Регистрация</Link>
             <Link className="header__link-auth header__link-auth_type_in">Войти</Link>
-            
+         
          </nav> */}
-         {/* <button className="header__menu-button" type="button"></button> */}
+         
         
-
-
-
       </header>
    );
 }
