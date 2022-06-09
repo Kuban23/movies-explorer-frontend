bab5abf69770f7e4e7fca374df231cabbb635c5a
Esin Andrew <kubanesin@gmail.com>
2022-06-09 22:04:28 +0300

    feat: Реализовал отк/закр sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,25 +5,40 @@ import { Link, NavLink } from 'react-router-dom';
 import icon from '../../images/profile_icon.svg';
 
 function Header() {
+
+   // Состояние sidebar
+   const [isSidebarOpen, setIsSidebarOpen] = React.useState(false)
+
+   // Открываю/закрываю sidebar
+   function handleOpenSidebar () {
+      setIsSidebarOpen(true);
+   }
+
+   function handleCloseSidebar () {
+      setIsSidebarOpen(false);
+   }
+
+
+
    return (
       <header className="header">
 
          <Link className="header__logo" to="/" />
 
-         {/* <div className="header__container_type_left">
+         {/* <div className="header__container_type_left"> */}
 
          <nav className="header__links">
-         <Link className="header__link-films" to="/movies">Фильмы</Link>
-         <Link className="header__link-films" to="/saved-movies">Сохранённые фильмы</Link>
+         <NavLink className="header__link-films" activeClassName="header__link-films_type_header_active" to="/movies" acti>Фильмы</NavLink>
+         <NavLink className="header__link-films" activeClassName="header__link-films_type_header_active" to="/saved-movies">Сохранённые фильмы</NavLink>
          </nav>
         <>
          <Link className="header__profile-link" to="/profile">
          <img className='header__profile-link-icon'src={icon} alt='Иконка профиля'/>
          </Link>
-         <button className="header__menu-button" type="button"></button>
+         <button className="header__menu-button" type="button" onClick={handleOpenSidebar}></button>
          </>
 
-         </div>    */}
+         {/* </div>    */}
 
 
          {/* <nav className="header__link-container">
@@ -31,8 +46,8 @@ function Header() {
             <Link className="header__link-auth header__link-auth_type_in" to="/signin">Войти</Link>
          </nav> */}
 
-         <nav className="header__link-sidebar header__link-sidebar_active">
-            <button className="header__link-sidebar-close-btn" type="button"></button>
+         <nav className={isSidebarOpen ? "header__link-sidebar_active" : "header__link-sidebar"}>
+            <button className="header__link-sidebar-close-btn" type="button" onClick={handleCloseSidebar}></button>
 
             <ul className="header__link-sidebar-container">
 
