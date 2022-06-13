24eba92075f1c5c60561cba4a231b3d886c17189
Esin Andrew <kubanesin@gmail.com>
2022-06-13 19:46:02 +0300

    feat:обавил код в Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,11 +1,12 @@
-
-import React from "react";
+import React from 'react'
 import './Preloader.css'
 
-function Preloader(){
-	return ( 
-<div></div>
-);
-}
+const Preloader = () => (
+  <div className="preloader">
+    <div className="preloader__container">
+      <span className="preloader__round" />
+    </div>
+  </div>
+)
 
-export default Preloader;
+export default Preloader 
